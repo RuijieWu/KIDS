97ffd2c1252e67fda4e69055e067dd7e00de3f7d
alex_xiao <alex_xiao@hust.edu.cn>
2024-07-17 19:49:23 +0800

    Revert "refactor: Add Kairos completion endpoint for getting completions in chat"
    This reverts commit ba631730fde2c57ef1ee838b0f918db05811f8df.

--- Backend/webserver/main.go
+++ Backend/webserver/main.go
@@ -45,7 +45,6 @@ func main() {
 	router.GET("/kairos/objects", Kairos.GetObjects)
 	router.GET("/kairos/aberration-statics", Kairos.GetAberrationStatics)
 	router.GET("/kairos/graph-visual", Kairos.GetGraphVisual)
-	router.POST("/kairos/completions", Kairos.GetCompletions)
 
 	router.POST("/blacklist/set-blacklist", Blacklist.SetBlackList)
 	router.GET("/blacklist/get-blacklist", Blacklist.GetBlackList)
